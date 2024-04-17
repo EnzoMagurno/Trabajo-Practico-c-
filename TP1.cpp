e1e297b5f5cf2b17077215fe750402f4fe3ae00e
EnzoMagurno <magurnoenzo31602@gmail.com>
2024-04-16 22:38:43 -0300

    working on activity 6,7

--- TP1.cpp
+++ TP1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main(){
 
     /* 1)
     int numero;
@@ -28,8 +28,8 @@ int main()
     }
     */
 
-    /* 3)
-     */
+    /* 
+    3)
     int numero;
     cout << "Ingrese un numero entero" << endl;
     cin >> numero;
@@ -41,5 +41,88 @@ int main()
             cout << i << endl;
         };
     }
-    return 0;
+    else
+    {
+        for (int i = numero; i <= (numero * 2); i++)
+        {
+            cout << i << endl;
+        }
+    }
+     */
+
+    /*
+    4)
+    int numero;
+    cout <<"Ingrese un numero entero"<< endl;
+    cin >> numero;
+    int suma=0;
+    for (int i = 0; i < numero; i++){
+        int numero;
+        cout<<"Ingrese un numero entero:"<<endl;
+        cin>>numero;
+        suma+=numero;
+    }
+    cout<<"La suma de numeros ingresados es " << suma <<endl;
+    */
+
+    /*
+    5)
+    int numero;
+    int suma=0;
+    cout <<"Ingrese un numero entero"<< endl;
+    cin >> numero;
+        for (int i = 0; i < numero; i++){
+        int numero;
+        cout<<"Ingrese un numero entero:"<<endl;
+        cin>>numero;
+        while(numero>0){
+        cout<<"Ingrese un numero mayor que 0:"<<endl;
+        cin>>numero;
+        }
+        suma+=numero;
+        }
+    cout<<"La suma de numeros ingresados es " << suma <<endl;
+    */
+   
+   /*
+   6)
+  int fib(int n){
+    if(n<2) {
+        return 0;
+    }
+    return fib(n-1)+fib(n-2);
+  }
+  cout<<fib(25)<<endl;
+   */
+   /*
+   7)
+  int aprobados,desaprobados,alumno,nota=0;
+  cout<<"Alumno: ";
+  cin>>alumno;
+  while (alumno!=0){
+    cout<<"Nota: ";
+    cin>>nota;
+    if(nota>4){
+        aprobados++
+    }else{
+        desaprobados++
+    }
+    cout<<"Alumno: ";
+    cin>>alumno
+  }
+  cout<<"Aprobados: "<<aprobados<<endl;
+  cout<<"Desaprobados: "<<desaprobados<<endl;
+   */  
+
+
+  /*
+  9)
+
+  */
+ std::queue<string> nombres;
+ for (int i = 0; i <=3; i++){
+    cout<<"Ingrese un nombre: "<<endl;
+ }
+ cout<<nombres;
+  return 0;
 }
